7dc904e0420ddef6d701713e114f886107817789
Vinu Jey <vj568@york.ac.uk>
2021-11-04 19:11:33 +0000

    Implemented random quote from JSON functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
+  const quotesURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
   const [quote, setQuote] = useState('The best revenge is massive success.')
   const [author, setAuthor] = useState('Frank Sinatra')
+  const [randomNumber, setRandomNumber] = useState('0')
+  const [quotesArray, setQuotesArray] = useState(null)
+
+  const fetchQuotes = async (url) => {
+    const response = await fetch(url)
+    const parsedJSON = await response.json()
+    setQuotesArray(parsedJSON.quotes)
+    console.log(quotesArray)
+  }
+
+  useEffect(() => {
+    fetchQuotes(quotesURL)
+  }, [quotesURL])
+
+  const generateRandomNumber = () => {
+    let randomInteger = Math.floor(Math.random()*quotesArray.length)
+    setRandomNumber(randomInteger)
+  }
 
   const switchQuoteAndAuthor = () => {
-    setQuote('I am the one don\'t weigh a ton')
-    setAuthor('Vinu Jey')
+    generateRandomNumber()
+    setQuote(quotesArray[randomNumber].quote)
+    setAuthor(quotesArray[randomNumber].author)
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        
         <p>
           "{quote}"
         </p>
