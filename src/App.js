216406875fb60ca449a41806c3c8b6a8d743e86e
VinuJ <severematrixoverload@gmail.com>
2021-11-04 16:12:00 +0000

    Added change quote and author button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [quote, setQuote] = useState('The best revenge is massive success.')
+  const [author, setAuthor] = useState('Frank Sinatra')
+
+  const switchQuoteAndAuthor = () => {
+    setQuote('I am the one don\'t weigh a ton')
+    setAuthor('Vinu Jey')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        
         <p>
-          Hello World
+          "{quote}"
         </p>
+        <p>- {author}</p>
+        <button onClick={switchQuoteAndAuthor}>Change quote</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
